1afbc06307fa0f9540646f8741543234085e9b00
emreyilmazbrs <166497823+emreyilmazbrs@users.noreply.github.com>
2026-03-09 14:40:43 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:40:03.510Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-09T11:40:44.244Z
+Commit: Resolve null pointer exception in user service
