6db9aefdba174bf6fa424e5106cf2b400b237bb0
emreyilmazbrs <166497823+emreyilmazbrs@users.noreply.github.com>
2026-03-09 14:44:36 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:40:48.645Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T11:44:36.698Z
+Commit: Fix CSS styling bug on mobile devices
