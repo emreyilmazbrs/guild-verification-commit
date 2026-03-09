d72e2820875b3434d6ec16e9360a83010e476337
emreyilmazbrs <166497823+emreyilmazbrs@users.noreply.github.com>
2026-03-09 14:13:39 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:13:28.151Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-09T11:13:39.884Z
+Commit: Patch security vulnerability in authentication
