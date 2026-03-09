47def9ff19803a88d4d4e6ce3b61a458b7118939
emreyilmazbrs <166497823+emreyilmazbrs@users.noreply.github.com>
2026-03-09 14:44:37 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:44:36.698Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-09T11:44:37.808Z
+Commit: Fix memory leak in event handlers
