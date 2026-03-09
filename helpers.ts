7f5926d60c72632601d45b57e568dd1ded6225bb
emreyilmazbrs <166497823+emreyilmazbrs@users.noreply.github.com>
2026-03-09 14:44:38 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:44:37.808Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-09T11:44:38.963Z
+Commit: Patch security vulnerability in authentication
